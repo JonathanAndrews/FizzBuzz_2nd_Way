ca9d0e294f0fcd05c482a908b5e76b75d7aa85d4
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2018-08-27 14:37:39 +0100

    Now works for the numnber 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,6 +2,8 @@ class Integer
   def fizzbuzz
     if self % 3 == 0
       return "fizz"
+    elsif self == 5
+      return "buzz"
     else
       return self
     end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,7 @@ describe "fizzbuzz" do
   it "returns 'fizz' for the number 9" do
     expect(9.fizzbuzz).to eq "fizz"
   end
+  it "return 'buzz' for the number 5" do
+    expect(5.fizzbuzz).to eq "buzz"
+  end
 end
